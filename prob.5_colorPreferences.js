a3ea1ce23a61b4c81a18dea0c193ccdd22fa2530
johnpat24 <johnpatrickandaquig10@gmail.com>
2025-03-21 22:26:36 +0800

    Add files via upload

---
+++ prob.5_colorPreferences.js
@@ -0,0 +1,9 @@
+// empty array
+let colors = []
+
+// for loop statement 
+for (let i = 0; i < 3; i++) {
+    let color = prompt(`enter color ${i + 1}:`);
+    colors.push(color);
+    console.log(colors);
+}
