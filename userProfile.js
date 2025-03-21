ba78d5add837a815f1ec235c7a2568b24308880c
johnpat24 <johnpatrickandaquig10@gmail.com>
2025-03-21 22:24:33 +0800

    Add files via upload

---
+++ userProfile.js
@@ -0,0 +1,8 @@
+//prompt or ask the user about the info.
+let FullName = prompt( "enter your first and last name: ");
+let age = parseInt(prompt("enter your age: "));
+let favoriteNumber = parseInt(prompt("enter your favorite number: "));
+let favoriteColor = prompt("enter your favorite color: ");
+
+//printing the output
+console.log(" the fullname is: " + FullName + "\n" + " the age is: " + age + "\n " + "the favorite number is: " + favoriteNumber + "\n" + " the favorite number is: " + favoriteColor);
